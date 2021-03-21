0bf867c5c141005f562d49ba98a2d7e2fd8f7a7f
HeartBtz <67628834+HeartBtz@users.noreply.github.com>
2021-03-21 16:40:44 +0100

    Curl Upload

---
+++ cmake-build-debug/CMakeFiles/Projet_Annuel.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/mnt/g/__RACINE/_Documents/ESGI/Cours Seb 2020-2021/Langage C/Projet Annuel/main.c" "/mnt/g/__RACINE/_Documents/ESGI/Cours Seb 2020-2021/Langage C/Projet Annuel/cmake-build-debug/CMakeFiles/Projet_Annuel.dir/main.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/Projet_Annuel.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Projet_Annuel.dir/main.c.o"
+  "Projet_Annuel"
+  "Projet_Annuel.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Projet_Annuel.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
